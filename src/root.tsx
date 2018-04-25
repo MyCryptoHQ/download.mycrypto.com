f07dc36a28eec3fecb6d33e2a5d3bc031469f864
james-prado <jamesprado7@gmail.com>
2018-04-25 16:17:29 -0400

    Add svg assets

--- src/root.tsx
+++ src/root.tsx
@@ -1,12 +1,8 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import App from 'app'
-import 'what-input'
-import 'sass/styles.scss'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from 'app';
+import 'what-input';
+import 'sass/styles.scss';
 
-
-const root = document.getElementById('root')
-ReactDOM.render(
-		<App />,
-	root
-)
+const root = document.getElementById('root');
+ReactDOM.render(<App />, root);
