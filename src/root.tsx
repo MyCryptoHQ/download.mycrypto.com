813ac36f9b0c9d1de34cdfbec253fb55617e94d7
Daniel Ternyak <dternyak@gmail.com>
2018-04-25 22:55:58 -0500

    Redesigned

--- src/root.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from 'app';
-import 'what-input';
-import 'sass/styles.scss';
-
-const root = document.getElementById('root');
-ReactDOM.render(<App />, root);
